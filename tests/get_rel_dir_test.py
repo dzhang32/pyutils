b8a11b19a57941bcc2cd6a2a226d3a01620326ac
David Zhang <dyzhang32@gmail.com>
2021-09-11 14:26:55 +0100

    also add test for get_rel_dir

--- tests/get_rel_dir_test.py
+++ tests/get_rel_dir_test.py
@@ -1,15 +1,14 @@
 import unittest
-from pyutils import get_rel_path
-import os
+from pyutils import get_rel_dir
 
 class TestStringMethods(unittest.TestCase):
 
     def test_rel_path(self):
-        self.assertEqual(get_rel_path("/somewhere/scripts/live/", "../../results/live/"),
+        self.assertEqual(get_rel_dir("/somewhere/scripts/live/", "../../results/live/"),
                          "/somewhere/results/live/")
 
         with self.assertRaises(TypeError):
-            get_rel_path("../../results/live/")
+            get_rel_dir("../../results/live/")
 
 if __name__ == '__main__':
     unittest.main()
